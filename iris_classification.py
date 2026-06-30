99883f03e467c7f62c4822ff4c038fa5efdf7d82
Garima Swami <garimaswami116@gmail.com>
2026-06-30 13:34:39 +0530

    Fix import error: cross_val_score from model_selection

--- iris_classification.py
+++ iris_classification.py
@@ -8,13 +8,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.metrics import confusion_matrix, classification_report, cross_val_score
+from sklearn.metrics import confusion_matrix, classification_report
 import warnings
 
 warnings.filterwarnings('ignore')
